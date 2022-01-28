5aacded807eab55a646f92e0beea30806c76a0e1
jb <jessb.epicodus@gmail.com>
2022-01-28 15:29:05 -0800

    add pizza constructor; passes test 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
-// --- Business Logic ---
+// Create a website for a pizza company where a user can choose one or more individual toppings (cheese, pepperoni, artichoke, anchovy, etc) and a size to order a pizza and see the final cost.
+// Create a pizza object constructor with properties for toppings and size.
+// Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this. 
+// Allow the user to choose toppings and size for the pizza they'd like to order.
 
+// --- Business Logic ---
+function Pizza (size, toppings, price) {
+  this.size = size;
+  this.toppings = toppings;
+  this.cost = 0;
+}
 
 // --- UI Logic ---
+let pizza = new Pizza ("lg", "sausage", "15");
