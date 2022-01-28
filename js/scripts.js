01d62f6002880917c06eb8ebd8479f78a62a7459
jb <jessb.epicodus@gmail.com>
2022-01-28 14:38:12 -0800

    initial constructors

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// --- Business Logic ---
+function Order () {
+  this.addedPizzas = {};
+  this.totalCost = 0;
+}
+
+function Pizza() {
+  this.size = size;
+  this.toppings = toppings;
+  this.price = price;
+}
+
+// --- UI Logic ---
